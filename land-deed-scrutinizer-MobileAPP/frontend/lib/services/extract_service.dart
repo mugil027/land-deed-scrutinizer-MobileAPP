a1ec4aa8ebb1ca871e223c06d2106d7fe80ccda9
mugil027 <mugil272000@gmail.com>
2025-07-25 12:01:21 +0530

    updateapi

--- land-deed-scrutinizer-MobileAPP/frontend/lib/services/extract_service.dart
+++ land-deed-scrutinizer-MobileAPP/frontend/lib/services/extract_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class ExtractService {
   static Future<Map<String, dynamic>> uploadPDF(File file) async {
-    var uri = Uri.parse("http://192.168.0.104:8000/extract"); // ← For Android emulator
+    var uri = Uri.parse("http://10.214.71.163:8000/extract"); // ← For Android emulator
     var request = http.MultipartRequest('POST', uri);
     request.files.add(await http.MultipartFile.fromPath('file', file.path));
 
